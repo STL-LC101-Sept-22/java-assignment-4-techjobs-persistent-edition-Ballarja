b832cd01102afa01afa6379548aa0307e02fdbcf
Ballarja <JamesBallard87@hotmail.com>
2023-03-11 23:48:40 -0600

    added new interfaces and updated EmployerController

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,43 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.Objects;
+
+@Entity
 public class Employer extends AbstractEntity {
+    @NotBlank(message = "This cannot be left empty")
+    @Size(min = 1, max = 150, message = "Max character length is 150")
+    private String location;
 
 
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Employer employer = (Employer) o;
+        return Objects.equals(location, employer.location);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), location);
+    }
+
+    @Override
+    public String toString() {
+        return "Employer{" +
+                "location='" + location + '\'' +
+                '}';
+    }
 }
